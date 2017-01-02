d9c73b23ba407a03c48336f78f461258d2fc3963
guilhermedelucas <guilhermedelucas@yahoo.com>
2017-01-02 15:03:10 +0100

    Major changes on Angular routing and app setting

---
+++ public/controllers/controllers.js
@@ -0,0 +1,18 @@
+(function () {
+
+    app.controller('HomeCtrl', function($rootScope, $location)
+    {
+       $rootScope.activetab = $location.path();
+    });
+
+    app.controller('AboutCtrl', function($rootScope, $location)
+    {
+       $rootScope.activetab = $location.path();
+    });
+
+    app.controller('LoginCtrl', function($rootScope, $location)
+    {
+       $rootScope.activetab = $location.path();
+    });
+
+})();
