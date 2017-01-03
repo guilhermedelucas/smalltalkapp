0553011266c3f6b1c1069fb26a855b94048af6eb
moaaz-ashour <ashour.m@live.de>
2017-01-03 17:29:25 +0100

    working on login and register

--- index.js
+++ index.js
@@ -1,23 +1,87 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
-
-//queries//
 var password = require("./password.json");
 var spicedPg = require('spiced-pg');
 var db = spicedPg('postgres:' + password.dbUser + ':' + password.dbPassword + '@localhost:5432/smalltalk');
-//queries//
+
+var hashingAndChecking = require ('./password/checking-hashing');
+
+var bodyParser = require('body-parser');
+var cookieSession = require('cookie-session');
+var cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.use(cookieSession({
+   secret: process.env.SESSION_SECRET || 'Help Dogs!',
+   maxAge: 1000 * 60 * 60 * 24 * 14
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
 
 
 app.use(express.static('public'));
 
-app.use(function(req,res, next){
-    console.log(req.url);
-    console.log(req.params.tag);
-    console.log(req.method);
-    console.log(req.path);
+app.use(function(req,res,next){
+   //  console.log(req.url);
+   //  console.log(req.params.tag);
+   //  console.log(req.method);
+   //  console.log(req.path);
     next();
 });
+
+app.post('/register', function(req, res){
+   hashingAndChecking.hashPassword(req.body.password).then(function(hashedPass){
+      var query = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING username';
+      var params = [req.body.username, req.body.email, hashedPass];
+      var fillUserTable = db.query(query, params);
+      fillUserTable.then(function(data){
+         req.session.user = {
+            username: data.rows[0].username
+         }
+         res.json({
+            success: true
+         })
+      })
+   }).catch(function(err){
+      console.log(err);
+   })
+});
+
+app.post('/login', function(req, res){
+   var query = 'SELECT username, password FROM users WHERE username = $1';;
+   var params = [req.body.username];
+   if (req.body.username && req.body.password) {
+      var getPass = db.query(query, params);
+      getPass.then(function(data){
+         console.log(data);
+         console.log(data.rows[0].username);
+         req.session.user = {
+            username: data.rows[0].username
+         }
+         var checkPass = hashingAndChecking.checkPassword(req.body.password, data.rows[0]['password']); //typedPass, dbPass
+         checkPass.then(function(data){
+            if(data === true) {
+               //response with json to redirect to homepage
+            } else if (data === false){
+               res.json({
+                  message: "Email or password is wrong! Please check and submit again."
+               })
+            }
+         }).catch(function(err){
+            console.log(err);
+         });
+      }).catch(function(err){
+      console.log(err);
+      });
+   }
+});
+
+app.get('*', function(req, res){
+   res.sendFile(__dirname + "/public/index.html");
+})
 
 app.listen(8080, function() {
     console.log('Listening')
--- public/controllers/logincontroller.js
+++ public/controllers/logincontroller.js
@@ -1,7 +1,47 @@
 (function () {
 
-    app.controller('AboutCtrl', function($rootScope, $location)
-    {
-       $rootScope.activetab = $location.path();
-    });
+   app.controller('LoginCtrl', function($rootScope, $scope, $location, $http)
+   {
+      $rootScope.activetab = $location.path();
+      //registration
+      $scope.getRegistrationData = function() {
+         console.log($scope.registrationPassword)
+         if (!$scope.registerUsername || !$scope.registerEmail || !$scope.registerPassword || !$scope.confirmedPassword){
+               $scope.registerFaliure = true;
+         } else {
+            var registeredUserData = {
+               username: $scope.registerUsername,
+               email: $scope.registerEmail,
+               password: $scope.registerPassword,
+            }
+            $http.post('/register', registeredUserData).then(function(res){
+               res.config.data = '';
+               //redirect $location
+            })
+         }
+         //check pasword matching
+         if ($scope.registerPassword !== $scope.confirmedPassword) {
+            $scope.passwordNotMatch = true;
+         } else {
+            $scope.passwordNotMatch = undefined;
+         }
+      }
+
+      //login
+      $scope.getLoginData = function() {
+         if(!$scope.loginUsername || !$scope.loginPassword) {
+            $scope.loginFaliure = true;
+         } else {
+            var loginUserData = {
+               username: $scope.loginUsername,
+               password: $scope.loginPassword
+            }
+            $http.post('/login', loginUserData).then(function(res){
+               res.config.data = '';
+               var message = res.data.message;
+               //redirect $location
+            });
+         }
+      }
+   })
 })();
