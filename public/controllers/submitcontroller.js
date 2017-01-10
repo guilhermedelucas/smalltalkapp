450e46e60f64d9be064f2807dc0dca83a794ce34
Guilherme Gomes de Lucas <guilhermedelucas@yahoo.com>
2017-01-10 11:21:18 +0100

    Update submitcontroller.js

--- public/controllers/submitcontroller.js
+++ public/controllers/submitcontroller.js
@@ -1,18 +1,25 @@
 (function () {
     app.controller('SubmitCtrl', function($rootScope, $location, $scope, $http)
     {   $rootScope.activetab = $location.path();
+
+        $http.get('submit/checksession').then(function(resp){
+            $scope.username = resp.data.username;
+        });
+
+        $scope.logged = true;
         $scope.submit = (formTitle, formUrl, formText) => {
-            if (!formTitle || !formUrl || !formText) {
-                $scope.submiterror =true;
+            console.log(formTitle, formUrl, formText);
+            if (!formTitle && (!formUrl || !formText)) {
+                $scope.submiterror = true;
             } else {
                 var data = {
+                    username: $scope.username,
                     title: formTitle,
                     url: formUrl,
                     text: formText
                 };
                 $http.post('/submit', data).then(function(resp){
                     $scope.posts = resp.data.posts;
-                    console.log($scope.posts);
                     $location.path('/home');
                 });
             }
